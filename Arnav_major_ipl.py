74bdf3d133d17ec88f0aaad49f88630bcf315fb2
arnav-verma22 <arn.verma22@gmail.com>
2020-11-17 07:11:12 +0530

    removing columns nd taking two teams

--- Arnav_major_ipl.py
+++ Arnav_major_ipl.py
@@ -9,3 +9,8 @@ import pandas as pd
 #def predict_score(bat_team, bowl_team):
 dataset = pd.read_csv("ipl2017.csv")
 
+#removed unnecessary columns
+columns_to_drop = ['date', 'wickets', 'overs', 'runs_last_5', 'wickets_last_5', 'striker', 'non-striker','runs']
+data = dataset.drop(columns = columns_to_drop)
+data = data[(data['bat_team'] == 'Chennai Super Kings') | (data['bowl_team'] == 'Royal Challengers Bangalore')]
+
