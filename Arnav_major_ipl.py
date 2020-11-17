cf3b6b541253c21650798f61969ab57b27c6040f
arnav-verma22 <arn.verma22@gmail.com>
2020-11-17 07:09:09 +0530

    importing dataset

---
+++ Arnav_major_ipl.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  6 06:44:28 2020
+
+@author: Arnav Verma
+"""
+import pandas as pd
+
+#def predict_score(bat_team, bowl_team):
+dataset = pd.read_csv("ipl2017.csv")
+
