3bb10fb96ba157b85b4e7e4ea72d18cff4476779
arnav-verma22 <arn.verma22@gmail.com>
2020-11-17 07:34:39 +0530

    apllying decision tree and random forst

--- chnges.py
+++ chnges.py
@@ -56,3 +56,11 @@ x = final_data.drop(['total'], axis = 1)
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+forest=RandomForestRegressor()
+forest.fit(x_train,y_train)
+y_pred = forest.predict(x_test)
+forest.score(x_test,y_test)
+forest.score(x_train,y_train)
+
